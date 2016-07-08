c92c12a309e30c7ffdaa8e6aea75bfb9e55c0ce9
Fábio Alves Ribeiro <contato.fabioalves@outlook.com>
2016-07-08 18:35:49 -0300

    08-07: Modificações 03 em main-admin.php. Criação de modal para menu Cadastrar.

--- OdontoTech/main-admin.php
+++ OdontoTech/main-admin.php
@@ -15,17 +15,13 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
         <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
-        
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        
         <!-- Bootstrap -->
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-        
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </head>
@@ -35,7 +31,6 @@
             <br>
             <div class="row cabecalhoP">
                 <header>Sistema Odontológico - Home</header>
-                
                 <!-- Expandable Textfield -->
                 <div class="topo">
                     <!-- Left aligned menu below button -->
@@ -46,8 +41,7 @@
                         <li disabled class="mdl-menu__item">Disabled Action</li>
                         <li class="mdl-menu__item">Yet Another Action</li>
                     </ul>
-                    
-                       <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
                         <label class="mdl-button mdl-js-button mdl-button--icon" for="sample6"> <i class="material-icons">search</i> </label>
                         <div class="mdl-textfield__expandable-holder">
                             <input class="mdl-textfield__input" type="text" id="sample6">
@@ -59,8 +53,35 @@
             <br>
             <div class="row menus row-centered">
                 <div class="col-md-3 text-center col-centered">
-                    <a href="#" class="hvr-shutter-out-horizontal cadastrar">
-                    <img src="images/cadastrar-render.png" class="cadastrar" alt="cadastrar" onmouseover="this.src='images/cadastrar-render-onmouse.png'" onmouseout="this.src='images/cadastrar-render.png'"></a>
+                    <a href="#m" class="hvr-shutter-out-horizontal cadastrar" data-toggle="modal" data-target="#myModal"> <img src="images/cadastrar-render.png" class="cadastrar" alt="cadastrar" onmouseover="this.src='images/cadastrar-render-onmouse.png'" onmouseout="this.src='images/cadastrar-render.png'"> </a>
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Cadastrar</h4> </div>
+                                <div class="modal-body">
+                                    <div class="modal-container">
+                                        <div class="row row-centered">
+                                            <div class="col-md-4 col-centered text-center dentista">
+                                                <a href="#m" class="hvr-shutter-out-horizontal cadastrar"> <img src="images/cadastrar-dentista-render.png" class="cadastrar" alt="cadastrar" onmouseover="this.src='images/cadastrar-dentista-render-onmouse.png'" onmouseout="this.src='images/cadastrar-dentista-render.png'"> </a>
+                                            </div>
+                                            <div class="col-md-4 col-centered text-center paciente">
+                                                <a href="#m" class="hvr-shutter-out-horizontal cadastrar"> <img src="images/cadastrar-paciente-render.png" class="cadastrar" alt="cadastrar" onmouseover="this.src='images/cadastrar-paciente-render-onmouse.png'" onmouseout="this.src='images/cadastrar-paciente-render.png'"> </a>
+                                            </div>
+                                            <div class="col-md-4 col-centered text-center funcionarios">
+                                                <a href="#m" class="hvr-shutter-out-horizontal cadastrar"> <img src="images/cadastrar-funcionarios-render.png" class="cadastrar" alt="cadastrar" onmouseover="this.src='images/cadastrar-funcionarios-render-onmouse.png'" onmouseout="this.src='images/cadastrar-funcionarios-render.png'"> </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-3 text-center col-centered">
                     <a href="#" class="hvr-shutter-out-horizontal"><img src="images/buscar-render.png" class="buscar" alt="buscar" onmouseover="this.src='images/buscar-render-onmouse.png'" onmouseout="this.src='images/buscar-render.png'"></a>
@@ -75,7 +96,7 @@
                     <a href="#" class="hvr-shutter-out-horizontal"><img src="images/relatorio-render.png" class="relatorio" alt="relatorio" onmouseover="this.src='images/relatorio-render-onmouse.png'" onmouseout="this.src='images/relatorio-render.png'"></a>
                 </div>
                 <div class="col-md-3 text-center col-centered">
-                    <a href="#" class="hvr-shutter-out-horizontal"><img src="images/procedimento-render.png" class="procedimento" alt="procedimento" onmouseover="this.src='images/receita-render-onmouse.png'" onmouseout="this.src='images/receita-render.png'"></a>
+                    <a href="#" class="hvr-shutter-out-horizontal"><img src="images/procedimento-render.png" class="procedimento" alt="procedimento" onmouseover="this.src='images/procedimento-render-onmouse.png'" onmouseout="this.src='images/procedimento-render.png'"></a>
                 </div>
             </div>
             <br>
